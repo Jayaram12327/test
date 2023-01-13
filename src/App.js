ec1056dca7f02df7825972ae300168026272e519
Jayaram12327 <jayaram12327@gmail.com>
2023-01-13 07:32:17 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Cards from './components/cards';
+
+
+function App() {
+  return (
+    <div className="App">
+    <Cards></Cards>
+   
+    </div>
+  );
+}
+
+export default App;
